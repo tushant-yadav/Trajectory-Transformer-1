7195e16d4970861b528ca30567fbc3e8252ee5ed
Tushant Yadav <56768158+tushant-yadav@users.noreply.github.com>
2021-04-13 18:28:05 +0530

    Coustom attention layer1

--- transformer/multihead_attention.py
+++ transformer/multihead_attention.py
@@ -19,17 +19,41 @@ class MultiHeadAttention(nn.Module):
         self.linears = clones(nn.Linear(d_model, d_model), 4)
         self.attn = None
         self.dropout = nn.Dropout(p=dropout)
-        self.conv1d = nn.Conv1d(7, 7, 3, 1, 1)
+        self.conv1d = nn.Conv1d(64, 64, 3, 1, 1)
 
+
+    def convo(self,node):
+        conv1=[]
+        conv1.append(self.conv1d(node[0]+node[1]))
+        conv1.append(self.conv1d(node[0]+node[1]+node[2]))
+        conv1.append(self.conv1d(node[2]+node[1]+node[3]))
+        conv1.append(self.conv1d(node[3]+node[2]+node[4]))
+        conv1.append(self.conv1d(node[4]+node[3]+node[5]))
+        conv1.append(self.conv1d(node[5]+node[4]+node[6]))
+        conv1.append(self.conv1d(node[6]+node[5]+node[7]))
+        conv1.append(self.conv1d(node[7]+node[6]))
+        return conv1  
+
+    def reunion(self, matrix ):
+        k = torch.unsqueeze(matrix[0],1)
+        for i in range(1,8):
+            k = torch.cat((k,torch.unsqueeze(matrix[i],1)),1)
+
+        return k
 
     def forward(self, query, key, value, mask=None):
         """
         Implements Figure 2
         """
-        node=[]
-        conv1=[]
+        #print('multihead entry:',np.shape(query))
+        #print('multihead entry:',np.shape(key))
+        #print('multihead entry:',np.shape(value))
+        nodekey=[]
+        nodequery=[]
+        nodevalue=[]
         splited =[]
         atn = []
+
         if mask is not None:
             # Same mask applied to all h heads.
             mask = mask.unsqueeze(1)
@@ -40,33 +64,80 @@ class MultiHeadAttention(nn.Module):
         key=torch.split(key,self.d_k,-1)
         value=torch.split(value,self.d_k,-1)
 
-        print('++++++++++++++++++++++++++ ++++++')
-
+        #print('++++++++++++++++++++++++++ ++++++')
+        #print(np.shape(query))
+        #print(np.shape(query[0]))
         #INPUT LAYER
+
+
+
+
         for i in range(0,self.h):
-            node.append(torch.cat((key[i],value[i],query[i]),-1))
-        print('=========================================== nodes formed')
-        print(np.shape(node[0]))
+            nodekey.append(torch.transpose(key[i],1,2))
+
+        for i in range(0,self.h):
+            nodequery.append(torch.transpose(query[i],1,2))
+
+        for i in range(0,self.h):
+            nodevalue.append(torch.transpose(value[i],1,2))
+
+        #print(np.shape(nodequery))
+        #print(np.shape(nodequery[0]))
+        #print('0000000000000000000000000000000000000000000000000000000000000')
+        nodekey   = self.convo(nodekey)
+        nodequery = self.convo(nodequery)
+        nodevalue = self.convo(nodevalue)
+
+
+
+        #print('=========================================== nodes formed')
+        #print(np.shape(nodequery))
+        #print(np.shape(nodequery[0]))
 
         #SET1 : CONV1D LAYERS
-        conv1.append(self.conv1d(node[0]+node[1]))
-        conv1.append(self.conv1d(node[0]+node[1]+node[2]))
-        conv1.append(self.conv1d(node[2]+node[1]+node[3]))
-        conv1.append(self.conv1d(node[3]+node[2]+node[4]))
-        conv1.append(self.conv1d(node[4]+node[3]+node[5]))
-        conv1.append(self.conv1d(node[5]+node[4]+node[6]))
-        conv1.append(self.conv1d(node[6]+node[5]+node[7]))
-        conv1.append(self.conv1d(node[7]+node[6]))
-        print(np.shape(conv1[0]))
-        print('********************************************* reaching to attn ')
+
+        #print(np.shape())
+        #print('********************************************* reaching to attn ')
         #attention
-        for i in range(0,self.h):
-            splited.append(torch.split(conv1[i],self.d_k,-1)) 
-        print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ splited')
-        print(np.shape(splited[0][0]))
-        for i in range(0,self.h):
-            atn[i] = attention(splited[i][2], splited[i][0], splited[i][1], mask=mask, dropout=self.dropout)
-        print('attention going+++++++++++++++++++++++++++++++++++++++')
-        x, self.attn = torch.cat((atn[0],atn[1],atn[2],atn[3],atn[4],atn[5],atn[6],atn[7]),-1)
-        print('attention gained &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
+        '''for i in range(0,self.h):
+            splited.append(torch.split(torch.transpose(conv1[i],1,2),self.d_k,-1))
+        '''
+        
+        k = torch.transpose(self.reunion(nodekey),2,3)
+        q = torch.transpose(self.reunion(nodequery),2,3)
+        v = torch.transpose(self.reunion(nodevalue),2,3)
+
+        #print(np.shape(k))
+        #print(np.shape(q))
+        #print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ splited')
+        #print(np.shape(splited))
+        x, self.attn = attention(q, k, v, mask=mask, dropout=self.dropout)
+        #print('x',np.shape(x))
+        #print('self.attn:' ,np.shape(self.attn))
+        #print('attention going+++++++++++++++++++++++++++++++++++++++')
+        # 3) "Concat" using a view and apply a final linear.
+        x = x.transpose(1, 2).contiguous().view(nbatches, -1, self.h * self.d_k)
+        #print('x1:',np.shape(x))
         return self.linears[-1](x)
+
+
+        '''Query  torch.Size([100, 8, 12, 64])
+        key  torch.Size([100, 8, 7, 64])
+        value torch.Size([100, 8, 7, 64])
+        Scores torch.Size([100, 8, 12, 7])
+        Query  torch.Size([100, 8, 12, 64])
+        key  torch.Size([100, 8, 12, 64])
+        value torch.Size([100, 8, 12, 64])
+        Scores torch.Size([100, 8, 12, 12])
+        Query  torch.Size([100, 8, 12, 64])
+        key  torch.Size([100, 8, 7, 64])
+        value torch.Size([100, 8, 7, 64])
+        Scores torch.Size([100, 8, 12, 7])
+        Query  torch.Size([100, 8, 12, 64])
+        key  torch.Size([100, 8, 12, 64])
+        value torch.Size([100, 8, 12, 64])
+        Scores torch.Size([100, 8, 12, 12])
+        Query  torch.Size([100, 8, 12, 64])
+        key  torch.Size([100, 8, 7, 64])
+        value torch.Size([100, 8, 7, 64])
+        Scores torch.Size([100, 8, 12, 7])'''
