5fb406ccbf594ce747328bca8f202bfe1b10e54f
Tushant Yadav <56768158+tushant-yadav@users.noreply.github.com>
2021-04-29 23:00:06 +0530

    Update decoder.py

--- transformer/decoder.py
+++ transformer/decoder.py
@@ -2,21 +2,30 @@
 # date: 2018-11-29 20:07
 import torch.nn as nn
 
-from .layer_norm import LayerNorm
 from .functional import clones
+from .layer_norm import LayerNorm
 
 
-class Decoder(nn.Module):
+class Encoder(nn.Module):
     """
-    Generic N layer decoder with masking.
+    Core encoder is a stack of N layers
     """
 
     def __init__(self, layer, n):
-        super(Decoder, self).__init__()
+        super(Encoder, self).__init__()
         self.layers = clones(layer, n)
         self.norm = LayerNorm(layer.size)
 
-    def forward(self, x, memory, src_mask, tgt_mask):
+    def forward(self, x, x_mask):
+        """
+        Pass the input (and mask) through each layer in turn.
+        """
+        x_list = []
+    
         for layer in self.layers:
-            x = layer(x, memory, src_mask, tgt_mask)
-        return self.norm(x)
+            x = layer(x, x_mask)
+            x_list.append(x)
+        
+        for i in range(0,len(x_list)):
+            x_list[i] = layer(x_list[i], x_mask)
+        return x_list
