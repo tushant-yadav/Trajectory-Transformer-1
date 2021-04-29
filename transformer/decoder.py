8716c1661236a21ae68379cb725a5689a739b7d6
Tushant Yadav <56768158+tushant-yadav@users.noreply.github.com>
2021-04-29 23:00:24 +0530

    Update decoder.py

--- transformer/decoder.py
+++ transformer/decoder.py
@@ -2,30 +2,29 @@
 # date: 2018-11-29 20:07
 import torch.nn as nn
 
-from .functional import clones
 from .layer_norm import LayerNorm
+from .functional import clones
 
 
-class Encoder(nn.Module):
+class Decoder(nn.Module):
     """
-    Core encoder is a stack of N layers
+    Generic N layer decoder with masking.
     """
 
     def __init__(self, layer, n):
-        super(Encoder, self).__init__()
+        super(Decoder, self).__init__()
         self.layers = clones(layer, n)
         self.norm = LayerNorm(layer.size)
 
-    def forward(self, x, x_mask):
-        """
-        Pass the input (and mask) through each layer in turn.
-        """
-        x_list = []
-    
+    def forward(self, x, memory, src_mask, tgt_mask):
+        i=0
         for layer in self.layers:
-            x = layer(x, x_mask)
-            x_list.append(x)
-        
-        for i in range(0,len(x_list)):
-            x_list[i] = layer(x_list[i], x_mask)
-        return x_list
+           
+            x = layer(x, memory[-1]+memory[i], src_mask, tgt_mask)
+            i=i+1
+        return self.norm(x)
+        #Decoder layer forward 
+        #x: torch.Size([100, 12, 512])  
+        #memory : torch.Size([100, 7, 512])  
+        #src_mask : torch.Size([100, 1, 7])  #
+        #tgt_mask : torch.Size([100, 12, 12])
